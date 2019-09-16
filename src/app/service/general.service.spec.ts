facd91530989bd3b3af67665b6ee3d7596a5bc6d
bhanuwhite <feather.white@gmail.com>
2019-09-16 17:56:07 +0530

    added case for get api

--- src/app/service/general.service.spec.ts
+++ src/app/service/general.service.spec.ts
@@ -1,12 +1,37 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { GeneralService } from './general.service';
+import mockData from './mock.data.json';
+import { APIConstant } from '../shared/constant'
 
 describe('GeneralService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [GeneralService],
+    imports: [HttpClientTestingModule]
+  }));
 
-  it('should be created', () => {
-    const service: GeneralService = TestBed.get(GeneralService);
-    expect(service).toBeTruthy();
-  });
+  describe(":", () => {
+    function setup() {
+      const generalService = TestBed.get(GeneralService);
+      const httpTestingController = TestBed.get(HttpTestingController);
+      return { generalService, httpTestingController };
+    }
+
+    it('it should return the total repo from github', () => {
+      const { generalService, httpTestingController } = setup();
+      generalService.get(APIConstant.url).subscribe(data => {
+        expect(data.data).toEqual(mockData);
+        expect(data.data.total_count).toBe(1625)
+      })
+      let req = httpTestingController.expectOne(APIConstant.url);
+
+      expect(req.request.method).toBe('GET');
+      req.flush({ data: mockData });
+
+    })
+    afterEach(() => {
+      const { httpTestingController } = setup();
+      httpTestingController.verify();
+    });
+  })
 });
